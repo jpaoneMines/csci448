42a6af0451f670f4ef28fda70e836918b950f878
jpaoneMines <jpaone@mines.edu>
2023-04-19 13:59:59 -0600

    put publishing in project gradle file

--- app/src/main/java/edu/mines/csci448/MainActivity.kt
+++ app/src/main/java/edu/mines/csci448/MainActivity.kt
@@ -8,8 +8,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import edu.mines.csci448.ui.compose.MultiStateSwitch
 import edu.mines.csci448.ui.theme.CSCI448Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +25,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val switchState = remember { mutableStateOf(0) }
+                    MultiStateSwitch(
+                        options = arrayOf(
+                            { Text(text = "One") },
+                            { Text(text = "Two") },
+                            { Text(text = "Three") }
+                        ),
+                        selectedIndex = switchState.value,
+                        onSelect = { switchState.value = it }
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CSCI448Theme {
-        Greeting("Android")
+        val switchState = remember { mutableStateOf(0) }
+        MultiStateSwitch(
+            options = arrayOf(
+                { Text(text = "One") },
+                { Text(text = "Two") },
+                { Text(text = "Three") }
+            ),
+            selectedIndex = switchState.value,
+            onSelect = { switchState.value = it }
+        )
     }
 }
